9ae6916d41513a0a9d62309478f3169fecf82ba0
Tsubasa Shimomura <tsubasa.shimomura@gmail.com>
2024-02-25 16:51:35 +0900

    delete unnecessary line

--- mk-outs.sh
+++ mk-outs.sh
@@ -7,4 +7,3 @@ echo "Hello there" > $OUTDIR/hello.txt
 echo "Hello"  "there" > $OUTDIR/hello2.txt
 echo -n "Hello  there" > $OUTDIR/hello1.n.txt
 echo -n "Hello" "there" > $OUTDIR/hello2.n.txt
-echo 'test'
